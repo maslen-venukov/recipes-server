a8427d534d90f77a3767a930bf0e21f371494856
Sergei <maslenv@bk.ru>
2021-03-08 16:32:37 +0500

    add cors

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import mongoose from 'mongoose';
+import cors from 'cors';
 import config from 'config';
 
 import userRouter from './routes/user.js';
@@ -24,6 +25,7 @@ mongoose.connect(DB_URL, dbConfig)
   .then(() => console.log('MongoDB connected'))
   .catch(e => console.log(e));
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('static'));
@@ -34,6 +36,6 @@ app.use('/api/meal', mealRouter);
 app.use('/api/favorites', favoritesRouter);
 app.use('/api/own', ownRouter);
 
-app.get('/', (req, res) => res.send('Hello from recipes API'));
+app.get('/', (req, res) => res.send('Yo` Recipes API'));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
